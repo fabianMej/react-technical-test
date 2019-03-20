a77832f4c77588fba5b361567fa45df5155f334b
Leison Fabian Mejia Garcia <VYG0165@intranet.vygts.com>
2019-03-20 12:22:09 -0500

    Prediseño de la pantalla de centros

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App container">
-        <div className="row">
+        <div className="row justify-content-center">
           <Centers></Centers>
         </div>
       </div>
--- src/components/Centers/Center/Center.tsx
+++ src/components/Centers/Center/Center.tsx
@@ -10,11 +10,14 @@ interface Props {
 const center: React.SFC<Props> = (props) => {
     return (
         <div className="card-center">
-            <section className="card-header">
+            <div className="header">
                 <h4>{props.centerModel.name}</h4>
                 <p>1 Miles from location * Kids from 2 to 11 years</p>
-            </section>
-            <section className="offers">
+            </div>
+            <div className="circle-div">
+                <img src={props.centerModel.profilePicture.url} className="circle-avatar" width="120" height="120"/>
+            </div>
+            <div className="offers">
                 <div className="row">
                     <div className="col-md-4">
                         <p className="freeOffer">First Hour Free</p>
@@ -26,16 +29,15 @@ const center: React.SFC<Props> = (props) => {
                         <p>Camps</p>
                     </div>
                 </div>
-            </section>
+            </div>
             <section className="reserve row justify-content-between">
-                <div className="col-md-4">
+                <div className="col-md-6 align-self-center">
                     <p><strong>$15.50</strong> PER HOUR FIRST CHILD</p>
                 </div>
                 <div className="col-md-4 align-self-center">
-                    <Button clicked={() => {}} text="Reserve Now"></Button>
+                    <Button clicked={() => {}} text="RESERVE NOW"></Button>
                 </div>
             </section>
-            
         </div>
     );
 }
